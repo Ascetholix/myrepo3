3aa37126550a774d9c6042a40bbc0c130ead43cf
Ascetholix <sangak@mail.ru>
2022-09-20 00:49:31 +0300

    tet

--- ex08/Program.cs
+++ ex08/Program.cs
@@ -1,54 +1,58 @@
 ﻿
 Console.Clear();
+//    void Obj(int[,] arg, int sn)
+// {
+//     int n = 0;
+//     if (sn == 1)
+//     {
 
+//         arg[3, 2] = 1;
+//         arg[4, 2] = 1;
+//         arg[4, 3] = 1;
+//         arg[5, 2] = 1;
+//     }
+//     else if (sn == 2)
+//     {
+
+//         arg[3, 2] = 1;
+//         arg[3, 3] = 1;
+//         arg[4, 3] = 1;
+//         arg[3, 4] = 1;
+//     }
+//     else if (sn == 3)
+//     {
+
+//         arg[3, 2] = 1;
+//         arg[3, 3] = 1;
+//         arg[4, 3] = 1;
+//         arg[2, 3] = 1;
+//     }
+//     else if (sn == 4)
+//     {
+//         n = 1;
+//         arg[3 - n, 2] = 1;
+//         arg[4 - n, 2] = 1;
+//         arg[4 - n, 3] = 1;
+//         arg[5 - n, 2] = 1;
+//     }
+//     else
+//     {
+//         arg[3, 2] = 1;
+//         arg[4, 2] = 1;
+//         arg[4, 3] = 1;
+//         arg[5, 2] = 1;
+//     }
+// }
+
+void Obj(int[,] arg, int sn)
+{
+    if (sn == 1) arg[3, 2 + sn] = 1;
+}
+
+int N = 0;
 int exit = 0;
 while (exit != 10)
 {
-
-    void Obj(int[,] arg, int sn)
-    {
-        int n = 0;
-        if (sn == 1)
-        {
-
-            arg[3, 2] = 1;
-            arg[4, 2] = 1;
-            arg[4, 3] = 1;
-            arg[5, 2] = 1;
-        }
-        else if (sn == 2)
-        {
-
-            arg[3, 2] = 1;
-            arg[3, 3] = 1;
-            arg[4, 3] = 1;
-            arg[3, 4] = 1;
-        }
-        else if (sn == 3)
-        {
-           
-            arg[3, 2] = 1;
-            arg[3, 3] = 1;
-            arg[4, 3] = 1;
-            arg[2, 3] = 1;
-        }
-        else if (sn == 4)
-        {
-            n = 1;
-            arg[3 - n, 2] = 1;
-            arg[4 - n, 2] = 1;
-            arg[4 - n, 3] = 1;
-            arg[5 - n, 2] = 1;
-        }
-        else
-        {
-            arg[3, 2] = 1;
-            arg[4, 2] = 1;
-            arg[4, 3] = 1;
-            arg[5, 2] = 1;
-        }
-    }
-
     int[,] tetris = new int[,]
     {
 {2, 0, 0, 0, 0, 0, 0, 0, 2 },
@@ -59,7 +63,7 @@ while (exit != 10)
 {2, 0, 0, 0, 0, 0, 0, 0, 2 },
 {2, 0, 0, 0, 0, 0, 0, 0, 2 },
 {2, 0, 0, 0, 0, 0, 0, 0, 2 },
-
+{2, 0, 0, 0, 0, 0, 0, 0, 2 },
     };
 
     void PrintArrya(int[,] argImage)   // Метод вывода матрицы
@@ -69,18 +73,23 @@ while (exit != 10)
             for (int j = 0; j < argImage.GetLength(1); j++)  // columns - столбцы
             {
                 if (argImage[i, j] == 2) Console.Write("|");
-                if (argImage[i, j] == 1) Console.Write("#");
+                if (argImage[i, j] == 1) Console.Write("▓");
                 else Console.Write(" ");
             }
             Console.WriteLine();
         }
     }
+    int EntCommand()
+    {
+        Console.Write("Для выхода 10. 1 - право, 2 -лево, 3 вниз, 4 - вверх: ");
+        return N = Convert.ToInt32(Console.ReadLine());
+    }
 
-    Console.Write("Для выхода 10. 1 - право, 2 -лево, 3 вниз, 4 - вверх: ");
-    int N = Convert.ToInt32(Console.ReadLine());
-
+    EntCommand();
     Obj(tetris, N);
+    Console.Clear();
 
     PrintArrya(tetris);
+
     exit = N;
 }
